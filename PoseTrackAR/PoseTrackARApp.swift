bb0e66f9b1da77e8d320d8e9f6a03ebb9aaac7d9
jeonSangMin <j3k2kk22@gmail.com>
2025-05-31 18:01:02 +0900

    Initial Commit

---
+++ PoseTrackAR/PoseTrackARApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseTrackARApp.swift
+//  PoseTrackAR
+//
+//  Created by 전상민 on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct PoseTrackARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
