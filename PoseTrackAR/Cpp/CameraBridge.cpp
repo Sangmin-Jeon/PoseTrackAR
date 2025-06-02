6dce8ba7d247a41c9e8405835a4169846a8e9448
jeonSangMin <j3k2kk22@gmail.com>
2025-06-03 02:29:41 +0900

    OpenCV 연동

--- PoseTrackAR/Cpp/CameraBridge.cpp
+++ PoseTrackAR/Cpp/CameraBridge.cpp
@@ -5,10 +5,15 @@
 //  Created by 전상민 on 6/3/25.
 //
 
-#include "CameraBridge.h"
 #include <stdio.h>
+#include "CameraBridge.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+
 
 void receive_intrinsics(struct Intrinsics_C intr) {
     printf("[C++] fx: %.2f, fy: %.2f, cx: %.2f, cy: %.2f, width: %d, height: %d\n",
            intr.fx, intr.fy, intr.cx, intr.cy, intr.width, intr.height);
+    
 }
